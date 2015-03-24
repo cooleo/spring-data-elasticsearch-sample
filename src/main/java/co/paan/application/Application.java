5dfedc950b3b1bb4eb5934e482b9e894caae2184
herotl2005 <nguyendanghung2008@gmail.com>
2015-03-24 09:18:21 +0700

    Remove comment
    Remove comment

--- src/main/java/co/paan/application/Application.java
+++ src/main/java/co/paan/application/Application.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration(exclude = {ElasticsearchConfiguration.class})
 public class Application {
     public static void main(String args[]){
-        //hello
         SpringApplication.run(Application.class);
     }
 }
